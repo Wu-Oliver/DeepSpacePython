f770f35652c34bbc9466897b2c5f0852c7d05930
Oliver Wu <oliverwulondon@gmail.com>
2019-04-09 16:54:13 -0400

    Uploading files

---
+++ src/python/frc/robot/Robot.py
@@ -0,0 +1,11 @@
+
+import wpilib
+import wpilib.drive
+from wpilib.robotbase import RobotBase
+
+class SpartanRobot(wpilib.TimedRobot):
+    
+    def robotInit(self):
+        return super().robotInit()
+
+    
